3f5185b287be215bfac48a479d6b214cccfdab8e
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-12-04 17:40:00 +1100

    Updated equations.

--- haskell/src/Game/SolarPower.hs
+++ haskell/src/Game/SolarPower.hs
@@ -2,44 +2,21 @@ module Game.SolarPower where
 
 import Data.SolarPower
 
-maxPanels :: Int {- m^2 -}
-maxPanels = 5000*5000
-
-targetEnergy :: Float {- KWh -}
-targetEnergy = 239.3e12
-
-panelUnitCost :: Float {- $ per m^2 -}
-panelUnitCost = 600
-
-panelEfficiency :: Float {- -}
-panelEfficiency = 0.4
-
-energyUnitPrice :: Float {- $ per KWh -}
-energyUnitPrice = 0.12
-
-panelCost :: Int -> Float {- $ -}
-panelCost panels = (fromIntegral panels * panelUnitCost)
-
-ghiEnergy :: Float {- MJ per m^2 -} -> Float {- KWh -}
-ghiEnergy ghi = ghi * panelEfficiency * 365 * 3.6
-
-energyRevenue :: Float -> Float {- $ -}
-energyRevenue = (* energyUnitPrice)
-
-panelsEnergy :: Int -> Float -> Float
-panelsEnergy panels' ghi = ghiEnergy $ fromIntegral panels * ghi
-	where panels = min panels' maxPanels
-
-panelsRevenue :: Int -> Float -> Float
-panelsRevenue panels ghi = energyRevenue energy 
-	where energy = min targetEnergy $ panelsEnergy panels ghi
-
-profit :: Float -> Float
-profit ghi = panelsRevenue maxPanels ghi  - panelCost maxPanels
+targetEnergy = 239.3e12 :: Float {- kWh -}
+energyUnitPrice = 0.12 :: Float {- $ per kWh -}
+farmEfficiency = 0.4 :: Float {- unitless -}
+farmArea = 5000 * 5000 :: Float {- m^2 -}
+farmCost = 2.5e9 :: Float {- $ -}
 
 score :: [SolarArray] -> [GHI] -> Float
-score arrays ghis = sum tilesWithPanels
-	where tilesWithPanels = [profit ghi
-	                         | GHI coords ghi <- ghis,
-	                           SolarArray coords' <- arrays,
-	                           coords == coords']
+score arrays ghis = revenue (sum energy) - sum cost
+	where (energy, cost) = unzip [(farmEnergy ghi, farmCost)
+	                              | GHI coords ghi <- ghis,
+	                                SolarArray coords' <- arrays,
+	                                coords == coords']
+
+revenue :: Float {- MJ per m^2 -} -> Float {- $ -}
+revenue energy = min energy targetEnergy * energyUnitPrice
+
+farmEnergy :: Float {- MJ per m^2 -} -> Float {- kWh -}
+farmEnergy ghi = ghi * 365 * farmArea * farmEfficiency * 3.6
